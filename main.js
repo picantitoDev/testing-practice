014da96053416e14605f8846058bcf2c0cf37634
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-02-16 00:38:31 -0500

    Capitalize Func Done

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 function capitalize(string) {
+  if (string.length === 0) {
+    return "no valid characters"
+  }
+
+  if (string.charAt(0) === " " || !isNaN(string.charAt(0))) {
+    let index = 0
+    let str = ""
+
+    for (let i = 0; i < string.length; i++) {
+      if (string.charAt(i) !== " " && isNaN(string.charAt(i))) {
+        index = i
+        break
+      }
+    }
+    str = string.slice(0, index)
+
+    return str + string.charAt(index).toUpperCase() + string.slice(index + 1)
+  }
+
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
-console.log(capitalize("test"))
+console.log(capitalize("   test"))
 module.exports = capitalize
